74976a1a29dc9a593d2e5bcb0deff4712555af31
ishan dutta <ishandutta2007@gmail.com>
2014-05-12 01:44:07 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,5 @@
+df <- read.table('household_power_consumption.txt', header = T, sep = ';', stringsAsFactors = F, na.strings = '?', skip = 66636, nrow = 2880)
+colnames(df) <- c('Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
+png('plot1.png')
+hist(df$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', ylim = c(0, 1200))
+dev.off()
